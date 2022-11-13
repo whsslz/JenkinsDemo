fa3e182de71c15bd805b69ed5909af4068d0c47a
森 <214011432@qq.com>
2022-11-13 16:33:36 +0800

    first commit message

---
+++ src/main/java/com/jenkins/test/HelloController.java
@@ -0,0 +1,13 @@
+package com.jenkins.test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "jsonObject hello ";
+    }
+}
